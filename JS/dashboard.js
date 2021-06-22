8cc13c2b5ff7202839d959621aae9b3a1f71887c
indrico <indrico.jowensen@gmail.com>
2021-06-22 17:03:53 +0700

    Tinggal Hapus data dan ubah Status

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,3 +1,18 @@
 const selesai = document.getElementById('selesai');
 const belumSelesai = document.getElementById('belum-selesai');
+let listData = [];
 
+window.addEventListener('load', () => {
+    let loaded = JSON.parse(localStorage.getItem('bookself'));
+    selesai.innerText = loaded.filter(checkSelesai).length;
+    belumSelesai.innerText = loaded.filter(checkBelumSelesai).length;
+    console.log(loaded);
+})
+
+function checkSelesai(status) {
+    return status.selesai === true;
+}
+
+function checkBelumSelesai(status) {
+    return status.selesai === false;
+}
--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,7 @@ const menuTambah = document.querySelector('.menu-tambah-buku');
 const dimContainer = document.querySelector('.dim-container');
 const tombolClose = document.getElementById('close');
 const form = document.getElementById('form');
+const clearSearch = document.querySelector('#clear-search')
 let angka = 1;
 let data = [];
 
@@ -27,22 +28,41 @@ form.addEventListener('submit', (e) => {
     insertData(e.target);
 })
 
-function insertData(listData) {
-    let id = listData[0].value;
-    let judul = listData[1].value;
-    let pengarang = listData[2].value;
-    let tahun = listData[3].value;
-    let selesai = listData[4].checked;
+clearSearch.addEventListener('click', () => {
+    searchBar.value = "";
+    document.getElementById('list-post').innerHTML = "";
+    angka = 1;
+    cleanData();
+    loadData();
+})
 
-    let rowData = { id, judul, pengarang, tahun, selesai };
+function cleanData() {
+    while (data.length > 0) {
+        data.pop();
+    }
+}
+
+function insertData(listData) {
+    let judul = listData[0].value;
+    let pengarang = listData[1].value;
+    let tahun = listData[2].value;
+    let selesai = listData[3].checked;
+
+    let rowData = { id: +new Date(), judul, pengarang, tahun, selesai };
     generateTable(rowData);
 
     data.push(rowData);
     updateLocalStorage();
 
-    // form.reset();
-    menuTambah.classList.remove('active');
-    dimContainer.classList.remove('active');
+    form.reset();
+    Swal.fire({
+        icon: 'success',
+        title: 'Berhasil...',
+        text: 'Data berhasil disimpan!',
+    }).then(() => {
+        menuTambah.classList.remove('active');
+        dimContainer.classList.remove('active');
+    })
 
 }
 
@@ -68,7 +88,7 @@ function generateTable(data) {
                         <td>${data.pengarang}</td>
                         <td>${data.tahun}</td>
                         <td>${data.selesai ? "Selesai" : "Belum Selesai"}</td>
-                        <td>Aksi</td>
+                        <td><i class='fas fa-trash' onclick='hapusData()'></i></td>
                     </tr>
                `;
     let tableRef = document.getElementById('table-list').getElementsByTagName('tbody')[0];
@@ -76,6 +96,36 @@ function generateTable(data) {
     tableRow.innerHTML = dataTable;
 
     angka++;
+}
+
+searchBar.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        angka = 1;
+        let keyword = e.target.value.toLowerCase();
+        let filterData = data.filter(function (item) {
+            return item.judul.toLowerCase().includes(keyword);
+        });
+
+        document.getElementById('list-post').innerHTML = "";
+        filterData.forEach((item) => {
+            generateTable(item);
+        })
+    }
+})
+
+function hapusData() {
+    console.log("Terhapus");
+}
+
+function findDataById(id) {
+    let index = 0;
+    for (item of data) {
+        if (item.id === id)
+            return index;
+        index++;
+    }
+
+    return -1;
 }
 
 loadData();
